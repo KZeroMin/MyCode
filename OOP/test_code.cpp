4962f2529ca7565662c18a26c7150710d57533c1
KZeroMin <zeromin1410@gmail.com>
2023-05-03 00:34:32 +0900

    first commit

---
+++ OOP/test_code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+    int result;
+
+    cin >> a >> b;
+
+    if(a > 0 && b <10){
+        result = a-b;
+        cout << result;
+    }
+
+    return 0;
+}
+
