41ab1254f6a57366d5a2debb0438c817fb3c118f
KZeroMin <zeromin1410@gmail.com>
2023-05-09 11:56:17 +0900

    Modifying the first code

--- oop_practice/sorting.cpp
+++ oop_practice/sorting.cpp
@@ -1,67 +1,75 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <random>
 
 using namespace std;
 
-// NOTICE
+// NOTICE(Todo)
 // 이 예제가 객체지향 공부로서 가치가 있으려면 여러 소팅 알고리즘이 데이터에 따라서
 // 최적화된 방법으로 선택되게 하면 좋을 것 같습니다. 이를테면, 
 // 1) quicksort가 대부분의 데이터가 정렬된 경우 O(n^2)이고
 // 2) 제한된 개수의 정수가 있을 경우 count sort O(n)
 // 등등
-class Sort{
+
+class Sort {
 public:
     Sort(int N);
+    void generate_random_numbers();
+    void add_numbers_to_vector();
     void sorting();
-    void get_input();
     void print_result();
 
 private:
-    int N_line;        // 변수명의 일관성 int line_count;
-    int arr[1001];     // no magic number
+    int line_count;  // 변수명의 일관성 int line_count (Check)
+    vector<int> number_container;  // array -> vector 형태로 바꾸면서, 크기를 동적으로 조절가능하도록 한다.
+    vector<int> random_numbers;
 };
 
-Sort::Sort(int N){
-    N_line = N;
+Sort::Sort(int N) : line_count(N) {
+    number_container.reserve(line_count);   // vector 메모리 할당
 }
 
-void Sort::get_input(){
-    for(int i=1; i<=N_line; i++){
-        cin >> arr[i];
+void Sort::generate_random_numbers() {
+    random_device rd;   // 난수 생성 장치
+    mt19937 gen(rd());   // 난수 생성 엔진
+    uniform_int_distribution<int> dis(1, 100);  // 범위 [1, 100]의 정수 난수 분포
+
+    for (int i = 0; i < line_count; i++) {
+        int num = dis(gen);
+        random_numbers.push_back(num);
     }
 }
 
-void Sort::sorting(){
-    sort(arr+1, arr+N_line+1);
-}
-
-// 아래 함수는 너무 문제가 많습니다. 절대 이렇게 코딩하면 안됩니다.
-// "모든 함수는 한 가지 일만 잘 해야한다"는 규칙에 완전히 위배됩니다.
-// print_result라는 이름하에
-// 1) input도 받고
-// 2) 정렬도 하고
-// 3) 사용자의 선택권이 없는 화면 출력
-//
-void Sort::print_result(){
-    get_input();
-    sorting();
-
-    for(int i=1; i<=N_line; i++){
-        cout << arr[i] << '\n';
+// 수정 필요
+void Sort::add_numbers_to_vector() {
+    for (int i = 0; i < line_count; i++) {
+        number_container.push_back(random_numbers[i]);
     }
 }
 
-// 이 메인도 좀 더 의도가 있도록 짤 수 있습니다.
-// 미리 간단한 난수를 발생시키거나, 10여개 정도의 정수를 직접 입력 등등해서 정렬 이후의 데이터를 관계 테스트 등
-int main(){
+void Sort::sorting() {
+    sort(number_container.begin(), number_container.end());
+}
+
+void Sort::print_result() {
+    for (int i = 0; i < line_count; i++) {
+        cout << number_container[i] << '\n';
+    }
+}
+
+int main() {
     int Num_line;
+    cout << "Enter the number of elements: ";
     cin >> Num_line;
 
     Sort sorting(Num_line);
+    sorting.generate_random_numbers();
+    sorting.add_numbers_to_vector();
+    sorting.sorting();
+
+    cout << "Sorted elements:\n";
     sorting.print_result();
 
     return 0;
-
 }
-
-
