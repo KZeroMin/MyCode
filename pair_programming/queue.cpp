78cc4e65899cabfef620085899a3a7a48ac3635a
KZeroMin <zeromin1410@gmail.com>
2023-05-16 18:27:49 +0900

    pair programming

---
+++ pair_programming/queue.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+template <typename T>
+class Queue
+{
+public:
+    void push(int in){
+        elements.push_back(in);
+    }
+
+    void pop()
+    {
+        if(elements.size() != 0)
+        {
+            elements.pop_front();
+        }
+    }
+
+    void front()
+    {
+        if(elements.size() != 0)
+        {
+            cout << elements.front() << endl;
+        }
+    }
+
+    void back()
+    {
+        if(elements.size() != 0)
+        {
+            cout << elements.back() << endl;
+        }
+    }
+
+    bool empty()
+    {
+        return elements.size() == 0;
+    }
+
+private:
+    deque<T> elements;
+};
+
+int main()
+{
+    Queue<int> int_queue;
+    int_queue.push(1);
+    int_queue.push(2);
+    int_queue.back();
+    int_queue.push(3);
+    int_queue.push(4);
+    int_queue.front();
+
+    int_queue.pop();
+    int_queue.front();
+    cout << int_queue.empty() << endl;
+
+    return 0;    
+}
