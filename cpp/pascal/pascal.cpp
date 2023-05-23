35c82c597020ed9aab117b243a8c25e555cf5250
KZeroMin <zeromin1410@gmail.com>
2023-05-24 01:18:50 +0900

    more optimize pascal

--- cpp/pascal/pascal.cpp
+++ cpp/pascal/pascal.cpp
@@ -6,11 +6,7 @@ using namespace std;
 class Pascal
 {
 public:
-    Pascal (int size)
-    {
-        size_of_pascal = size;
-        pascal_elements.resize(size_of_pascal, vector<int>(size_of_pascal, 0));
-    }
+    Pascal (int size) : size_of_pascal(size), pascal_elements(size, vector<int>(size, 0)) {} // use member initializer
 
     void calculate_combination()
     {
@@ -30,11 +26,12 @@ public:
     {
         string pascal_string;
 
-        for (int row = 0; row < size_of_pascal; row++)
+        for (const auto& row : pascal_elements)
         {
-            for (int column = 0; column <= row; column++)
+            for (const auto& element : row)
             {
-                pascal_string = pascal_string + to_string(pascal_elements[row][column]) + " ";
+                if (element != 0) pascal_string = pascal_string + to_string(element) + " ";  
+                else break;
             }
             pascal_string += '\n';
         }
