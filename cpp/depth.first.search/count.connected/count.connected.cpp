1d51961ab4f4d7adec69170de615fcf5b6e6ce2a
KZeroMin <zeromin1410@gmail.com>
2023-05-28 21:00:27 +0900

    change main, assert

--- cpp/depth.first.search/count.connected/count.connected.cpp
+++ cpp/depth.first.search/count.connected/count.connected.cpp
@@ -1,11 +1,12 @@
 #include "count.connected.hpp"
+#include <cassert>
 
 int main()
 {
     Map map = make_map_from_file();
     auto component = count_component(map);
-
-    cout << component;
+    
+    assert(component == 2);
 
     return 0;
 }
