cfe2265bc591eebe2ec809f1d50e24f6effa5b99
KZeroMin <zeromin1410@gmail.com>
2023-06-05 12:18:56 +0900

    more optimize

---
+++ build/CMakeFiles/sort_by_age.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kimmi/Desktop/MyCode/algorithm/sorting/sort.by.age/main.cpp" "CMakeFiles/sort_by_age.dir/main.cpp.obj" "gcc" "CMakeFiles/sort_by_age.dir/main.cpp.obj.d"
+  "C:/Users/kimmi/Desktop/MyCode/algorithm/sorting/sort.by.age/util/file.io.cpp" "CMakeFiles/sort_by_age.dir/util/file.io.cpp.obj" "gcc" "CMakeFiles/sort_by_age.dir/util/file.io.cpp.obj.d"
+  "C:/Users/kimmi/Desktop/MyCode/algorithm/sorting/sort.by.age/util/sort.cpp" "CMakeFiles/sort_by_age.dir/util/sort.cpp.obj" "gcc" "CMakeFiles/sort_by_age.dir/util/sort.cpp.obj.d"
+  "C:/Users/kimmi/Desktop/MyCode/algorithm/sorting/sort.by.age/util/util.cpp" "CMakeFiles/sort_by_age.dir/util/util.cpp.obj" "gcc" "CMakeFiles/sort_by_age.dir/util/util.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sort_by_age.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort_by_age.dir/main.cpp.obj"
+  "CMakeFiles/sort_by_age.dir/main.cpp.obj.d"
+  "CMakeFiles/sort_by_age.dir/util/file.io.cpp.obj"
+  "CMakeFiles/sort_by_age.dir/util/file.io.cpp.obj.d"
+  "CMakeFiles/sort_by_age.dir/util/sort.cpp.obj"
+  "CMakeFiles/sort_by_age.dir/util/sort.cpp.obj.d"
+  "CMakeFiles/sort_by_age.dir/util/util.cpp.obj"
+  "CMakeFiles/sort_by_age.dir/util/util.cpp.obj.d"
+  "libsort_by_age.dll.a"
+  "sort_by_age.exe"
+  "sort_by_age.exe.manifest"
+  "sort_by_age.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort_by_age.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
